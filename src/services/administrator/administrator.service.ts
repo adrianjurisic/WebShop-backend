650ab41be175910becaf59d27500f2474c27ab36
Adrian Jurisic <adrianjurisic1601@gmail.com>
2024-11-04 19:43:34 +0100

    Implement a global API response mechanism

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { error } from 'console';
 import { Administrator } from 'entities/administrator.entity';
+import { resolve } from 'path';
 import { identity } from 'rxjs';
 import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
 
 
@@ -18,11 +21,17 @@ export class AdministratorService {
         return this.administrator.find();
     }
 
-    getById(administratorId: number): Promise<Administrator> {
+    async getById(administratorId: number): Promise<Administrator | ApiResponse> {
+        let admin: Administrator = await this.administrator.findOne({ where: { administratorId } });
+        if(admin === null){
+            return new Promise((resolve)=>{
+                resolve(new ApiResponse("error", -1002));
+            });
+        }
         return this.administrator.findOne({ where: { administratorId } });
     }
 
-    add(data: AddAdministratorDto): Promise <Administrator>{
+    add(data: AddAdministratorDto): Promise <Administrator | ApiResponse>{
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
@@ -32,19 +41,30 @@ export class AdministratorService {
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHashString;
 
-        return this.administrator.save(newAdmin);
+        return new Promise((resolve) =>{
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            })
+        })
     }
 
-    async editById(administratorId: number, data: EditAdministratorDto): Promise<Administrator>{
+    async editById(administratorId: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse>{
         let admin: Administrator = await this.administrator.findOne({ where: { administratorId } });
+
+        if(admin === null){
+            return new Promise((resolve)=>{
+                resolve(new ApiResponse("error", -1002));
+            });
+        }
 
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
         const passwordHashString = passwordHash.digest('hex').toUpperCase();
-
         admin.passwordHash = passwordHashString;
-
         return this.administrator.save(admin);
     }
 }
